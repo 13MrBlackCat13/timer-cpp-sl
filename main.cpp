3191d58337dda191a5715806baf62ee844c4ca95
Михаил <69348201+13MrBlackCat13@users.noreply.github.com>
2021-09-20 17:56:25 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int timer;
+    cin >> timer;
+    timer++;
+    while(timer--) {
+        if (timer != 0){
+        if(timer % 5 != 0){
+            cout << timer << endl;
+        } else {
+            cout << timer << endl;
+            cout << "Beep" << endl;
+        }
+            }else {
+            return 0;
+        }
+    }
+    return 0;
+}
